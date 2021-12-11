d6e81e5dbc60c8faf718ecfdfa3ed779381a2182
fgarancini <f.garancini15@gmail.com>
2021-12-11 13:01:45 -0300

    Express app

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,13 @@
+const express = require("express");
 const dotenv = require("dotenv");
-const { Octokit, App } = require("octokit");
-
 dotenv.config({ path: "./.env" });
 
-const octoKit = new Octokit({ auth: process.env.GIT_TOKEN });
+const app = express();
+const port = process.env.PORT || 3000;
 
-const HelloUser = async (octoKit) => {
-    const {
-      data: { login },
-    } = await octoKit.rest.users.getAuthenticated();
-    console.log("Hello, %s", login);
-}
+app.use(express.json());
 
-HelloUser(octoKit);
+app.get("/", (req, res) => res.send("Hello World!"));
 
-const GetRepo = async (octoKit) => {
-    const repo = await octoKit.request("GET /repos/fgarancini/git-uroboros/commits");
-    console.log(repo);
-}
 
-GetRepo(octoKit);
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
