30d1f3dd96deb565c70a2b855b8ef98a445292cb
fgarancini <f.garancini15@gmail.com>
2021-12-12 17:29:59 -0300

    More styling

--- Client/git-uroboros/src/Components/Profile/CardProfile.js
+++ Client/git-uroboros/src/Components/Profile/CardProfile.js
@@ -4,19 +4,19 @@ import "./CardProfile.css";
 
 const CardProfile = (props) => {
   return (
-      <Card className="text-center card-profile" style={{width:'18rem'}}>
-        <Card.Img variant="top" src={props.item.avatar_url} />
-        <Card.Body>
-          <Card.Title>{props.item.name}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary" href={props.item.html_url} >
-            My full profile!
-          </Button>
-        </Card.Body>
-      </Card>
+    <Card className="text-center card-profile" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.item.avatar_url} />
+      <Card.Body>
+        <Card.Title>{props.item.name}</Card.Title>
+        <Card.Text>
+          Hello! My name is Franco and you can follow the progress of this repo
+          and its commits!
+        </Card.Text>
+        <Button variant="primary" href={props.item.html_url}>
+          My full profile!
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
